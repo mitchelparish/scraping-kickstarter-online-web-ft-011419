32bec15927eaf3db6bf3ad36acc3a2c1a144b6db
Mitchel Parish <mitchelparish@gmail.com>
2019-02-06 00:31:50 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-    binding.pry
+  
   # projects = {}
   #
   # # kickstarter.css("li.project.grid_4").each do |project|
@@ -29,3 +29,4 @@ def create_project_hash
   #
   # projects
 end
+  binding.pry
